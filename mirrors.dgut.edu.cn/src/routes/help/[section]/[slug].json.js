fa734bb3bc66a3737aef1b9ea9d7ef485592aca9
Chuang Zhu <genelocated@yandex.com>
2021-07-23 05:03:49 +0800

    help/[section]/[slug].json 404

--- mirrors.dgut.edu.cn/src/routes/help/[section]/[slug].json.js
+++ mirrors.dgut.edu.cn/src/routes/help/[section]/[slug].json.js
@@ -3,7 +3,14 @@ import marked from 'marked';
 
 export async function get(request) {
 	const { section, slug } = request.params;
-	const md = await fs.promises.readFile(`help/${section}/${slug}.md`, 'utf-8');
+	let md;
+	try {
+		md = await fs.promises.readFile(`help/${section}/${slug}.md`, 'utf-8');
+	} catch (err) {
+		return {
+			status: 404
+		}
+	}
 	const html = marked(md);
 	return {
 		status: 200,
