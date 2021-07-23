fa3ed2564838e2a35c5de5bcb08189207d1f23a4
Chuang Zhu <genelocated@yandex.com>
2021-07-24 00:20:15 +0800

    linux.dgut.edu.cn initial commit

--- linux.dgut.edu.cn/svelte.config.js
+++ linux.dgut.edu.cn/svelte.config.js
@@ -3,7 +3,6 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		// Where compiled JS and CSS are stored
 		appDir: 'assets/app',
 		files: {
 			// fs: public/ -> web: /
