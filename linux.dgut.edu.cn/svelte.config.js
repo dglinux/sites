537febd54e61dd5cad3c523fe353b3dff011cfea
Chuang Zhu <genelocated@yandex.com>
2021-07-28 07:31:56 +0800

    [home] add Dockerfile

--- linux.dgut.edu.cn/svelte.config.js
+++ linux.dgut.edu.cn/svelte.config.js
@@ -1,8 +1,19 @@
+import node from '@sveltejs/adapter-node';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: node({
+			esbuild(defaultOptions) {
+				return {
+					...defaultOptions,
+					// Let's bundle all our production depencencies
+					external: ['fsevent']
+				};
+			}
+		}),
 		appDir: 'assets/app',
 		files: {
 			// fs: public/ -> web: /
